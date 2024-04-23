c6ecc88b482616de698ca7eb0da4d0b2fca20cb9
QaldeK <qaldek@gmx.com>
2024-04-23 18:42:43 +0200

    fir typescript

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,7 +1,15 @@
+import AsyncAlpine from "async-alpine";
 import 'alpinejs'
-// import 'htmx'
+// import 'htmx'  import AsyncAlpine from "async-alpine";
+import Alpine from "alpinejs";
+
+
+
 
 export default function initGlobalScripts() {
     // Initialisation d'Alpine.js
+    AsyncAlpine.init(Alpine);
     Alpine.start();
+window.Alpine = Alpine;
+
 }
