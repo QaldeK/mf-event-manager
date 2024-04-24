ed62df4adb367e2ed4ca6ae182549bc3bb1af383
QaldeK <qaldek@gmx.com>
2024-04-24 03:05:44 +0200

    debug? cloudflare adaptater

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import alpinejs from "@astrojs/alpinejs";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpinejs(), tailwind()],
   output: "server",
+  adapter: cloudflare()
 });
